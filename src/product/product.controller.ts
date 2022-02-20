b3e897cc8bf7f8ab442ca13b92ce86f6837aece1
yj-leee <yj.leee1010@gmail.com>
2022-02-20 16:31:37 +0900

    add docker

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from "@nestjs/common";
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+  @Get()
+  getProduct() {
+    return 'This is product controller';
+  }
+}
